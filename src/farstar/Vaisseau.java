2094fb0f386e03c9b51790b7aa07aedcedd9d479
AL <antoine.lebel.65@gmail.com>
2017-01-25 19:50:06 -0500

    test

---
+++ src/farstar/Vaisseau.java
@@ -0,0 +1,27 @@
+package farstar;
+
+public class Vaisseau extends Transportable {
+    
+    public Vaisseau(String nom, int masse, int volume) {
+        super(nom, masse, volume);
+    }
+    
+    public void localiser(Transportable element) {
+        String typeElement = element.getClass().getSimpleName();
+        
+        switch(typeElement) {
+            case "Arme":
+                //GET LIST VAISSEAU ARME
+                //Localiser en foreach
+            case "Transport":
+                //GET LIST VAISSEAU TRANSPORT
+                //Localiser en foreach
+            case "Container":
+                //GET LIST CONTAINER
+                //Localiser en foreach
+            default:
+                //GET LIST TRANSPORTABLE
+                //Localiser en foreach
+        }
+    }
+}
